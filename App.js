e1e993027725024e07398d65c977d6556bd80686
MELHAFEDI <muhammadelhafedi@gmail.com>
2022-11-20 18:34:30 +0000

    Image Picker

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Component } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+
+    const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+    const [image, setImage] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+            setHasGalleryPermission(galleryStatus.status === 'granted');
+        })();
+    }, []);
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 4],
+            quality: 1,
+        });
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            setImage(result.uri);
+        }
+    };
+
     return (
         <View style={styles.container}>
-            <Text>Hello Again</Text>
+            <Button title="Select Image" onPress={() => pickImage()} />
+            {image && <Image source={{ uri: image }} style={{ width: 400, height: 400 }} />}
+            <StatusBar style='auto' />
         </View>
     );
 }
@@ -15,6 +44,6 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'center'
     },
-})
+});
