13c6ee19488331e8066928381819077c10da1baa
MELHAFEDI <muhammadelhafedi@gmail.com>
2022-11-20 21:15:47 +0000

    Image Picker

--- App.js
+++ App.js
@@ -1,49 +1,69 @@
-import React, { useState, useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
-export default function App() {
-
-    const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
-    const [image, setImage] = useState(null);
-
-    useEffect(() => {
-        (async () => {
-            const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
-            setHasGalleryPermission(galleryStatus.status === 'granted');
-        })();
-    }, []);
-
-    const pickImage = async () => {
-        let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
-            allowsEditing: true,
-            aspect: [4, 4],
-            quality: 1,
-        });
-
-        console.log(result);
-
-        if (!result.cancelled) {
-            setImage(result.uri);
-        }
+export default class App extends React.Component {
+    state = {
+        image: null,
     };
 
-    return (
-        <View style={styles.container}>
-            <Button title="Select Image" onPress={() => pickImage()} />
-            {image && <Image source={{ uri: image }} style={{ width: 400, height: 400 }} />}
-            <StatusBar style='auto' />
-        </View>
-    );
+    selectCamera = async () => {
+        
+        const { cancelled, uri } = await ImagePicker.launchImageLibraryAsync({
+            aspect: [3,3],
+            allowsEditing: true,
+        });
+        if (!cancelled) this.setState({ image: uri });
+    };
+
+    pickImage = async () => {
+     
+        const { cancelled, uri } = await ImagePicker.launchCameraAsync({
+            allowsEditing: false,
+        });
+        this.setState({ image: uri });
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image style={styles.image} source={{ uri: this.state.image }} />
+                <View style={styles.row}>
+                    <Button onPress={this.selectCamera}>Gallery</Button>
+                    <Button onPress={this.pickImage}>Camera</Button>
+                </View>
+            </View>
+        );
+    }
 }
 
+const Button = ({ onPress, children }) => (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Text style={styles.text}>{children}</Text>
+    </TouchableOpacity>
+);
+
 const styles = StyleSheet.create({
+    text: {
+        fontSize: 18,
+    },
+    row: { flexDirection: 'row' },
+    image: {
+        width: 350,
+        height: 350,
+        backgroundColor: 'darkgray',
+        borderRadius: 40,
+    },
+    button: {
+        padding: 12,
+        margin: 20,
+        backgroundColor: 'lightgray',
+        borderRadius: 20,
+    },
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: 'white',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
     },
 });
